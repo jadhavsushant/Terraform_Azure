6ba710ac58d96be55989f391da1c5373c6d7938e
sushantjadhav <47061845+jadhavsushant@users.noreply.github.com>
2020-10-20 23:24:20 +0530

    udpated var.tf
    updated var.tf - removed the provider details

--- tf_basic/var.tf
+++ tf_basic/var.tf
@@ -1,21 +1,17 @@
 variable "sub_id" {
-  type = string
-  default = "ea13738b-8e3e-4cda-a65f-4c76fc2f99bb"
+
 }
 
 variable "ten_id" {
-  type = string
-  default = "2b7f4f5b-c184-478c-8398-2228b450807c"
+ 
 }
 
 variable "cli_id" {
-  type = string
-  default = "0a2c2f71-d237-4665-bd04-baca7ada3077"
+
 }
 
 variable "cli_sec" {
-  type = string
-  default = "wRtsq~q4Dt82mjPj5wBaYxmvE9txYxQith"
+
 }
 
 variable "rgname" {
